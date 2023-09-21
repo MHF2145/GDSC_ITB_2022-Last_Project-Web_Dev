bc96440bd517367a4ae7ccf2b19e0408886e3d13
MHF2145 <117990315+MHF2145@users.noreply.github.com>
2023-09-21 09:46:57 +0700

    Progress back to 2%
    I guess gonna stick with local host :')

--- scripts/searchresult.js
+++ scripts/searchresult.js
@@ -3,24 +3,40 @@ document.addEventListener("DOMContentLoaded", function () {
     const query = urlParams.get("query");
 
     if (query) {
-        // Define the URL to your custom API endpoint
-        const apiUrl = `https://your-api-url.com/gethtml?query=${query}`;
+        // Define an array of possible file paths
+        const filePaths = [
+            `pokemons/sinnoh/${query}.html`,
+            `pokemons/kanto/${query}.html`,
+            `pokemons/johto/${query}.html`,
+            `pokemons/hoenn/${query}.html`,
+        ];
 
-        // Fetch and display the content from your custom API
-        fetch(apiUrl)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("File not found");
-                }
-                return response.text();
-            })
-            .then((htmlContent) => {
-                const resultContainer = document.getElementById("search-result");
-                resultContainer.innerHTML = htmlContent;
-            })
-            .catch((error) => {
+        // Initialize a variable to track if the file was found
+        let fileFound = false;
+
+        // Function to attempt fetching a file
+        const tryFetchFile = (filePath) => {
+            return fetch(filePath)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("File not found");
+                    }
+                    return response.text();
+                })
+                .then((htmlContent) => {
+                    const resultContainer = document.getElementById("search-result");
+                    resultContainer.innerHTML = htmlContent;
+                    fileFound = true; // Mark as found if successful
+                })
+                .catch((error) => {});
+        };
+
+        // Try fetching files from each path until one is found
+        Promise.all(filePaths.map(tryFetchFile)).then(() => {
+            if (!fileFound) {
                 handleNoQuery();
-            });
+            }
+        });
     } else {
         handleNoQuery();
     }
